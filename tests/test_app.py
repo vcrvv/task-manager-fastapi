3e0585a344269a4300cb4eb9df47cbb5f65921e2
vini <viniciusoliveira09_@Outlook.com>
2025-01-29 11:06:56 -0400

    Configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_tasks.app import app
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app) # Arrange - prepara o ambiente
+
+    response = client.get('/') # Act - chama o Sistema Sob Teste(o que sera testado)
+
+    assert response.status_code == HTTPStatus.OK # Assert - verifica se a respota é o que é esperado
+    # no caso a resposta esperada é status 200
+    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+    # no caso um json com esses dados
