523ab3ef5919ead6774b197b17ff913361eb975e
vini <viniciusoliveira09_@Outlook.com>
2025-01-29 21:35:34 -0400

    Adicionada a primeira migração com Alembic. Criada a tabela de usuarios.

--- fast_tasks/app.py
+++ fast_tasks/app.py
@@ -12,6 +12,7 @@ database = []
 def read_root():
     return {'message': 'Olá Mundo!'}
 
+
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
@@ -26,15 +27,22 @@ def read_users():
     return {'users': database}
 
 
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    return database[user_id - 1]
+
+
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
+def update_user(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
     
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
-    
-    return user_with_id
+    return database[user_id - 1] 
 
 
 @app.delete('/users/{user_id}', response_model=Message)
--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,6 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app) # Arrange - prepara o ambiente
 
     response = client.get('/') # Act - chama o Sistema Sob Teste(o que sera testado)
-
     assert response.status_code == HTTPStatus.OK # Assert - verifica se a respota é o que é esperado
     # no caso a resposta esperada é status 200
     assert response.json() == {'message': 'Olá Mundo!'} # Assert
@@ -46,25 +45,61 @@ def test_read_users(client):
         ]
     }    
 
+def test_get_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }     
+    
+def test_get_user_not_found(client):
+    response = client.get('/users/100')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+    
 def test_update_user(client):
     response = client.put(
         '/users/1',
         json={
-            'username': 'bob',
-            'email': 'bob@example.com',
+            'username': 'alice',
+            'email': 'alice@example.com',
             'password': 'mynewpassword',
         },
     )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
+        'username': 'alice',
+        'email': 'alice@example.com',
         'id': 1,
     }
 
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/100',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail':'User not found'}
+                           
 def test_delete_user(client):
     response = client.delete('/users/1')
     
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/100')
+    
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail':'User not found'}
+
