b976dfa62426f80cffa122936235b2337822a4f2
vini <viniciusoliveira09_@Outlook.com>
2025-02-02 14:49:51 -0400

    Adicionando PostgreSQL

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from fastapi.testclient import TestClient
 
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
-from sqlalchemy.pool import StaticPool
+from testcontainers.postgres import PostgresContainer
 
 from fast_tasks.app import app
 from fast_tasks.database import get_session
@@ -27,13 +27,18 @@ def client(session):
     app.dependency_overrides.clear()
 
 
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:latest', driver='psycopg') as postgres:    
+        
+        _engine =  create_engine(postgres.get_connection_url())
+        
+        with _engine.begin():
+            yield _engine
+
+
 @pytest.fixture
-def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+def session(engine):   
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
